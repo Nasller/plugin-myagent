2d7685503953110dcaff44750a1ea059f1a7e777
Nasller <nasller@qq.com>
2021-12-27 15:19:10 +0800

    myagent

--- src/main/java/com/nasller/myagent/MyPluginEntry.java
+++ src/main/java/com/nasller/myagent/MyPluginEntry.java
@@ -3,6 +3,7 @@ package com.nasller.myagent;
 import com.janetfilter.core.Environment;
 import com.janetfilter.core.models.FilterRule;
 import com.janetfilter.core.plugin.MyTransformer;
+import com.janetfilter.core.plugin.PluginConfig;
 import com.janetfilter.core.plugin.PluginEntry;
 import com.nasller.myagent.crack.BITransformer;
 import com.nasller.myagent.crack.StrTransformer;
@@ -13,23 +14,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyPluginEntry implements PluginEntry {
+    private static final String PLUGIN_NAME = "MyAgent";
+    private static final String PLUGIN_REMOTE_CONFIG = "MyAgent";
     private final List<MyTransformer> transformers = new ArrayList<>();
 
     @Override
-    public void init(Environment environment, List<FilterRule> filterRules) {
+    public void init(Environment environment, PluginConfig pluginConfig) {
         transformers.add(new BITransformer());
         transformers.add(new StrTransformer());
-        if(filterRules != null && filterRules.size() > 4 && filterRules.get(0).test("REMOTE")){
+        List<FilterRule> filterRules = pluginConfig.getBySection(PLUGIN_REMOTE_CONFIG);
+        if(filterRules != null && filterRules.size() > 4){
             //1.jumpServerUrl,2.指令,3.默认对应dev资源，4.默认对应pre或pro资源
-            List<FilterRule> rules = filterRules.subList(1, 5);
-            transformers.add(new CommandInnerTransformer(rules));
-            transformers.add(new ProcessInnerTransformer(rules));
+            transformers.add(new CommandInnerTransformer(filterRules));
+            transformers.add(new ProcessInnerTransformer(filterRules));
         }
     }
 
     @Override
     public String getName() {
-        return "MyAgent";
+        return PLUGIN_NAME;
     }
 
     @Override
